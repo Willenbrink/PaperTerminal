ff2094e201064fb47f444c7b017056b49dc847dc
Sebastian Willenbrink <sebastian.willenbrink@tum.de>
2019-04-11 21:42:34 +0200

    Add cursor and realize that a faulty driver caused all the hangups

--- ioctl.c
+++ ioctl.c
@@ -1,4 +1,5 @@
 #include <sys/ioctl.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
 
@@ -11,14 +12,15 @@ void set_screen_dimensions(int row, int col)
   struct winsize wbuf;
   wbuf.ws_row = row;
   wbuf.ws_col = col;
-  int fd = open("/dev/vcs", 0);
+  int fd = open("/dev/tty1", 0);
 
   ioctl(fd, TIOCSWINSZ, &wbuf);
+  close(fd);
 }
 
 void get_screen_dimensions()
 {
-  int fd = open("/dev/vcs", 0);
+  int fd = open("/dev/tty1", 0);
 
   ioctl(fd, TIOCGWINSZ, &wbuf);
   printf("%d - %d\n", wbuf.ws_row, wbuf.ws_col);
--- term.ml
+++ term.ml
@@ -1,8 +1,3 @@
-(*
-       0x00005413   TIOCGWINSZ       struct winsize *
-       0x00005414   TIOCSWINSZ       const struct winsize *
-   *)
-
 open Ctypes
 open Foreign
 
@@ -20,18 +15,23 @@ let get_term_dim () =
   funer "getx" (void @-> returning int) (), funer "gety" (void @-> returning int) ()
 
 module Screen = struct
-  type 'a t = 'a array array
+  type 'a matrix = 'a array array
+  type 'a t = { t : 'a matrix; prev : 'a matrix option}
 
-  let create cols rows value : 'a t = Array.make_matrix cols rows value
-  let set t c r v =
+  let create cols rows value : 'a t =
+    (* In docs: x then y but we want to access each row all at once -> first rows then cols
+       The getRow basically reads one value along the x dimension of the array , which corresponds to a column
+       but we want rows instead of columns*)
+    {t = Array.make_matrix rows cols value; prev = None}
+  let set {t; _} c r v =
     let rows = Array.length t in
     let cols = Array.length t.(0) in
-    if r >= rows || c >= cols then Printf.printf "Accessing %i:%i with max %i:%i\n" r c rows cols
+    if r < 0 || c < 0 || r >= rows || c >= cols then Printf.printf "Accessing %i:%i with max %i:%i\n" r c rows cols
     else
     t.(r).(c) <- v
-  let get t c r = t.(r).(c)
-  let getRow t r = t.(r) |> Array.to_list
-  let getRows (t : 'a t) =
+  let get {t; _} c r = t.(r).(c)
+  let getRow {t; _} r = t.(r) |> Array.to_list
+  let getRows {t; _} =
     Array.map (Array.to_list) t |> Array.to_list
 
 end
@@ -42,50 +42,57 @@ let read_vcs () =
   let cols = input_byte fd in
   let x = input_byte fd in
   let y = input_byte fd in
-  ignore (x + y);
-  let text = Screen.create cols rows ' ' in
-  let modifiers = Screen.create cols rows 0 in
+  (*TODO resize instead of creating it completely new*)
   let bytes () =
+    let text = Screen.create cols rows ' ' in
+    let modifiers = Screen.create cols rows 0 in
     let rec worker x y =
       try
-        let byte1,byte2 = input_byte fd, input_byte fd in
-        Screen.set text x y (char_of_int byte1);
-        Screen.set modifiers x y byte2;
         if y >= rows then text,modifiers
         else if x >= cols then worker 0 (y+1)
-        else worker (x+1) y
-      with End_of_file -> text,modifiers
+        else
+          let byte1,byte2 = input_byte fd, input_byte fd in
+          Screen.set text x y (char_of_int byte2);
+          Screen.set modifiers x y byte1;
+          worker (x+1) y
+      (* This should never occur! We've just read the size and expect it to be true!*)
+      with End_of_file -> raise End_of_file
     in
     worker 0 0
   in
   let (text,mods) = bytes () in
+
+  Screen.set text (cols -1) 0 (char_of_int (Random.int 2 + 31));
+  (* x,y is the cursor position *)
+  Screen.set text x y (char_of_int 0x7F);
+
   ignore mods;
   let () = close_in fd in
   text
 
-let read_vcs () =
-  Screen.create 82 26 'c'
-
 let main () =
+  set_term_dim 37 100;
   EPD.init () |> ignore;
   EPD.clear EPD.bg;
   EPD.display_buffer_all EPD.White;
   let rec loop prevlines =
     let text = read_vcs () in
-    let lines : string list = ["bla";"blub"] in
     let lines : string list =
       Screen.getRows text
       |> List.map (List.map (fun c -> String.make 1 c))
-      |> List.map (String.concat "") in
+      |> List.map (String.concat "")
+    in
     let linesChanged =
-    List.mapi (fun i s -> i,s) lines
-    |> List.fold_left (fun a (i,s) ->
-                    let line = match nth_opt prevlines i with None -> "" | Some line -> line in
-                    if s <> line then a+1 else a) 0
-  in
+      List.mapi (fun i s -> i,s) lines
+      |> List.fold_left (fun a (i,s) ->
+          let line = match nth_opt prevlines i with None -> "" | Some line -> line in
+          if s <> line then a+1 else a) 0
+    in
 
     let update_line i s =
-      EPD.draw_text (0, i * 16) s;
+      (try
+         EPD.draw_text (0, i * 16) s
+       with Invalid_argument x -> print_endline ("Exception: " ^ x));
       if linesChanged < 3 then EPD.display_buffer ((0, i * 16),(799, i * 16 + 16)) EPD.Fast
     in
     List.iteri (fun i s -> match nth_opt prevlines i with
@@ -96,17 +103,8 @@ let main () =
       ) lines;
     flush_all ();
     (if linesChanged >= 3 then EPD.display_buffer_all EPD.Fast);
-    (*loop lines*)
-    ()
+    loop lines
   in
   loop []
 
-;;
-main ()
-(*
-;;
-
-set_term_dim 10 10;
-let x,y = get_term_dim () in
-Printf.printf "%d - %d" x y
-   *)
+let () = main ()
