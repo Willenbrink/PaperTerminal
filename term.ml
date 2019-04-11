739767e61984aeb36b75f32759396fe2b2a0d680
Sebastian Willenbrink <sebastian.willenbrink@tum.de>
2019-04-11 23:12:18 +0200

    Change to a character based refresh This is for the time being a well performing implementation and further improvements will probably only provide marginal speedups. Next up is benchmarking and UTF-8.

--- term.ml
+++ term.ml
@@ -14,26 +14,39 @@ let get_term_dim () =
   let () = funer "get_screen_dimensions" (void @-> returning void) () in
   funer "getx" (void @-> returning int) (), funer "gety" (void @-> returning int) ()
 
-module Screen = struct
-  type 'a matrix = 'a array array
-  type 'a t = { t : 'a matrix; prev : 'a matrix option}
+module Matrix = struct
+  type 'a t = 'a array array
 
   let create cols rows value : 'a t =
     (* In docs: x then y but we want to access each row all at once -> first rows then cols
        The getRow basically reads one value along the x dimension of the array , which corresponds to a column
        but we want rows instead of columns*)
-    {t = Array.make_matrix rows cols value; prev = None}
-  let set {t; _} c r v =
+    Array.make_matrix rows cols value
+  let set t c r v =
     let rows = Array.length t in
     let cols = Array.length t.(0) in
     if r < 0 || c < 0 || r >= rows || c >= cols then Printf.printf "Accessing %i:%i with max %i:%i\n" r c rows cols
     else
     t.(r).(c) <- v
-  let get {t; _} c r = t.(r).(c)
-  let getRow {t; _} r = t.(r) |> Array.to_list
-  let getRows {t; _} =
+  let get t c r = t.(r).(c)
+  let getRow t r = t.(r) |> Array.to_list
+  let getRows t =
     Array.map (Array.to_list) t |> Array.to_list
 
+  let mapi f t =
+    Array.mapi (fun row_index row -> Array.mapi (fun col_index char -> f col_index row_index char) row) t
+
+  let iteri f t =
+    Array.iteri (fun row_index row -> Array.iteri (fun col_index char -> f col_index row_index char) row) t
+
+  let fold f_row f_col a b (t : 'a t) = (* Fold row by row, left to right*)
+    let fold_col = Array.fold_left (fun a char -> f_col a char) in
+    let fold_row = Array.fold_left (fun a row -> f_row a (fold_col b row)) in
+    fold_row a t
+
+  let diff t1 t2 =
+    let t_index = mapi (fun x y c -> (x,y,c)) t1 in
+    fold (@) (fun a (x,y,c) -> if get t2 x y = c then a else (x,y)::a) [] [] t_index
 end
 
 let read_vcs () =
@@ -43,28 +56,26 @@ let read_vcs () =
   let x = input_byte fd in
   let y = input_byte fd in
   (*TODO resize instead of creating it completely new*)
-  let bytes () =
-    let text = Screen.create cols rows ' ' in
-    let modifiers = Screen.create cols rows 0 in
+  let text,mods =
+    let text = Matrix.create cols rows ' ' in
+    let modifiers = Matrix.create cols rows 0 in
     let rec worker x y =
       try
         if y >= rows then text,modifiers
         else if x >= cols then worker 0 (y+1)
         else
           let byte1,byte2 = input_byte fd, input_byte fd in
-          Screen.set text x y (char_of_int byte2);
-          Screen.set modifiers x y byte1;
+          Matrix.set text x y (char_of_int byte2);
+          Matrix.set modifiers x y byte1;
           worker (x+1) y
       (* This should never occur! We've just read the size and expect it to be true!*)
       with End_of_file -> raise End_of_file
     in
     worker 0 0
   in
-  let (text,mods) = bytes () in
 
-  Screen.set text (cols -1) 0 (char_of_int (Random.int 2 + 31));
   (* x,y is the cursor position *)
-  Screen.set text x y (char_of_int 0x7F);
+  Matrix.set text x y (char_of_int 0x7F);
 
   ignore mods;
   let () = close_in fd in
@@ -75,36 +86,36 @@ let main () =
   EPD.init () |> ignore;
   EPD.clear EPD.bg;
   EPD.display_buffer_all EPD.White;
-  let rec loop prevlines =
+  let rec loop prevText =
     let text = read_vcs () in
     let lines : string list =
-      Screen.getRows text
+      Matrix.getRows text
       |> List.map (List.map (fun c -> String.make 1 c))
       |> List.map (String.concat "")
     in
-    let linesChanged =
-      List.mapi (fun i s -> i,s) lines
-      |> List.fold_left (fun a (i,s) ->
-          let line = match nth_opt prevlines i with None -> "" | Some line -> line in
-          if s <> line then a+1 else a) 0
-    in
 
-    let update_line i s =
-      (try
-         EPD.draw_text (0, i * 16) s
-       with Invalid_argument x -> print_endline ("Exception: " ^ x));
-      if linesChanged < 3 then EPD.display_buffer ((0, i * 16),(799, i * 16 + 16)) EPD.Fast
+    let chars_changed = Matrix.diff text prevText in
+
+    let update_char (x,y) =
+      Matrix.get text x y
+      |> EPD.draw_char (x * 8, y * 16)
     in
-    List.iteri (fun i s -> match nth_opt prevlines i with
-        | None -> update_line i s
-        | Some line -> match s <> line with
-          | true -> update_line i s
-          | false -> ()
-      ) lines;
-    flush_all ();
-    (if linesChanged >= 3 then EPD.display_buffer_all EPD.Fast);
-    loop lines
+    let () = List.iter update_char chars_changed in
+    let dirty_area =
+      List.fold_left
+        (fun area (x,y) -> match area with
+           | None -> Some ((x,y),(x,y))
+           | Some ((lx,ly),(hx,hy)) -> Some ((min lx x, min ly y),(max hx x, max hy y)))
+        None
+        chars_changed
+    |> function None -> None | Some ((lx,ly),(hx,hy)) -> Some ((lx * 8, ly * 16),((hx+1) * 8, (hy+1) * 16))
+    in
+    (match dirty_area with
+    | None -> ()
+    | Some dirty_area -> EPD.display_buffer dirty_area EPD.Fast);
+
+    loop text
   in
-  loop []
+  loop (Matrix.create 100 37 (char_of_int 0))
 
 let () = main ()
