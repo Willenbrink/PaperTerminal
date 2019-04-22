a5d042cd2a50111481788e04f612a93e12140485
Sebastian Willenbrink <sebastian.willenbrink@tum.de>
2019-04-22 12:30:25 +0200

    Improve drawing algorithm The results so far are satisfactory but this can be improved further. Essentially we want to implement a flood fill starting at every changed char. Trying out Github features... Closes #4

--- term.ml
+++ term.ml
@@ -49,6 +49,63 @@ module Matrix = struct
     fold (@) (fun a (x,y,c) -> if get t2 x y = c then a else (x,y)::a) [] [] t_index
 end
 
+module type Draw =
+sig
+  val f : (int * int) list -> ((int * int) * (int * int)) list
+end
+
+module BoundingBox : Draw =
+struct
+  let f points =
+    List.fold_left
+      (fun area (x,y) -> match area with
+         | None -> Some ((x,y),(x,y))
+         | Some ((lx,ly),(hx,hy)) -> Some ((min lx x, min ly y),(max hx x, max hy y)))
+      None
+      points
+    |> function None -> [] | Some area -> [area]
+end
+
+module Naive : Draw =
+struct
+  let f points = List.rev_map (fun p -> p,p) points
+end
+
+module Merge : Draw = (* Merge neighbouring dirty areas to one, continue until no further merge is possible. Could be improved by allowing non-dirty areas in the merge*)
+struct
+  let f points =
+    let areas = List.map (fun p -> p,p) points in
+    let rec worker acc xs = match xs with
+      | [] -> acc
+      | x::xs ->
+        let area_new,xs,changed =
+          List.fold_left
+            (fun ((((x1,y1),(x2,y2)) as area), xs, changed) ((x1',y1'),(x2',y2')) ->
+               let margin = 2 in
+               let within_margin (x1,y1) (x2,y2) = x2 <= x1 + margin && x2 >= x1 - margin && y2 <= y1 + margin && y2 >= y1 - margin in
+               let upper_left = within_margin (x1,y1) (x1',y1') in
+               let lower_right = within_margin (x2,y2) (x2',y2') in
+
+               let inside = x1' >= x1 && y1' >= y1 && x2' <= x2 && y2' <= y2 in
+               if inside then area, xs, changed
+               else
+                 let x1,y1 = if upper_left then min x1 x1', min y1 y1' else x1,y1 in
+                 let x2,y2 = if lower_right then max x2 x2', max y2 y2' else x2,y2 in
+                 let area' = (x1,y1),(x2,y2) in
+                 if area <> area' then area', xs, true
+                 else area', (((x1',y1'),(x2',y2'))::xs), changed
+            ) (x,[],false) xs
+        in
+        if changed
+        then worker acc (area_new::xs)
+        else worker (area_new::acc) xs
+
+    in
+    worker [] areas
+
+end
+
+
 let read_vcs () =
   let fd = open_in "/dev/vcsa1" in
   let rows = input_byte fd in
@@ -89,26 +146,22 @@ let init () =
 
 let rec loop prevText =
   let text = read_vcs () in
-  let chars_changed = Matrix.diff text prevText in
-
   let update_char (x,y) =
     Matrix.get text x y
     |> EPD.draw_char (x * 8, y * 16)
   in
-  let () = List.iter update_char chars_changed in
-  let dirty_area =
-    List.fold_left
-      (fun area (x,y) -> match area with
-         | None -> Some ((x,y),(x,y))
-         | Some ((lx,ly),(hx,hy)) -> Some ((min lx x, min ly y),(max hx x, max hy y)))
-      None
-      chars_changed
-    |> function None -> None | Some ((lx,ly),(hx,hy)) -> Some ((lx * 8, ly * 16),((hx+1) * 8, (hy+1) * 16))
+  let display dirty_areas =
+    (*
+    List.iter (fun ((x1,y1),(x2,y2)) -> Printf.printf "%i-%i : %i-%i\t" y1 x1 y2 x2) dirty_areas;
+    if List.length dirty_areas <> 0 then print_endline "";
+    *)
+    List.iter (fun ((x1,y1),(x2,y2)) -> EPD.display_buffer ((x1*8,y1*8),(x2*8+8,y2*16+16)) EPD.Fast) dirty_areas
   in
-  (match dirty_area with
-   | None -> ()
-   | Some dirty_area -> EPD.display_buffer dirty_area EPD.Fast);
 
+  let chars_changed = Matrix.diff text prevText in
+  let () = List.iter update_char chars_changed in
+  let dirty_areas = Merge.f chars_changed in
+  let () = display dirty_areas in
   loop text
 
 let main () =
